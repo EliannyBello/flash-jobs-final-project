a81a5444a96a5ab32512706c0e3f223ac1451af6
AnahiVera <vera.anahi.93@gmail.com>
2024-10-02 13:03:28 -0300

    cambiado

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="fixed-bottom">
             <h1>
-                I'm a Footer
+                I'm a Footer pero diferente
             </h1>
         </div>
     )
