ae4bc769d9d618cb143397baf99fe233b3a95389
fgamester <falvarez.munoz@hotmail.es>
2024-10-04 22:58:34 -0300

    AppContext imported in main.jsx, primitive logout navbar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,43 +1,82 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../context/GlobalContext";
+import { FaSearch, FaRegQuestionCircle } from "react-icons/fa";
+import '../styles/NavBar.css'
 
 const NavBar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider"/></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+    const context = useContext(Context);
+
+    const LogoutNavbar = () => (
+        <div className="container-fluid d-flex justify-content-between">
+            <Link className="navbar-brand" to='/' >Logo</Link>
+            <form className="d-flex d-md-none" role="search">
+                <div className="input-group">
+                    <input type="text" className="form-control input-search" placeholder="Search..." aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <button className="btn btn-search" type="button" id="btn-search">
+                        <FaSearch className="search-icon" />
+                    </button>
+                </div>
+            </form>
+            <div className="user-access d-block d-md-none">
+                <h5>
+                    Login/Sign In
+                </h5>
+            </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nabvarGeneralOptions"
+                aria-controls="nabvarGeneralOptions" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-between" id="nabvarGeneralOptions">
+                <ul className="navbar-nav mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><hr className="dropdown-divider" /></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <form className="d-flex d-none d-md-block" role="search">
+                        <div className="input-group">
+                            <input type="text" className="form-control input-search" placeholder="Search..." aria-label="Recipient's username" aria-describedby="button-addon2" />
+                            <button className="btn btn-search" type="button" id="btn-search">
+                                <FaSearch className="search-icon" />
+                            </button>
+                        </div>
                     </form>
+                </ul>
+
+                <div className="d-flex align-items-center">
+                    <div className="user-access d-none d-md-block me-3">
+                        <h5>
+                            Login/Sign In
+                        </h5>
+                    </div>
+                    <FaRegQuestionCircle className="fs-4" />
                 </div>
             </div>
+        </div>
+    )
+
+    const setNavBar = () => {
+        if (context.logged) return <div></div>
+        else return <LogoutNavbar />
+    }
+
+    useEffect(() =>{
+        console.log(context);
+    },[])
+
+    return (
+        <nav className="navbar navbar-expand-md navbar-light-mode">
+            {setNavBar()}
         </nav>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './Layout.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import { AppContext } from './context/GlobalContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AppContext>
     <App />
-  </StrictMode>,
+  </AppContext>,
 )
