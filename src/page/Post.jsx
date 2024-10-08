a3989b0f9843894d7b92de1997702a5cc4d55cdb
fgamester <falvarez.munoz@hotmail.es>
2024-10-08 11:59:50 -0300

    navbar fixed top

--- src/page/Post.jsx
+++ src/page/Post.jsx
@@ -67,7 +67,7 @@ const Post = () => {
     }
 
     return (
-        <div className="container-fluid mt-3">
+        <div className="container-fluid mt-3 pt-5">
             <div className="row g-3">
                 <div className="col-12 col-lg-4">
                     <div className="card">
