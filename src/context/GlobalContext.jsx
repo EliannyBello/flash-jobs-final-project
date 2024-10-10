28e4b0fc1e84381de72292c2e2e53f65f7c47721
fgamester <falvarez.munoz@hotmail.es>
2024-10-10 15:30:51 -0300

    problems solved

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,31 +1,27 @@
 import React, { createContext, useState, useEffect } from "react";
-
-
 export const Context = createContext(null)
-
-
-
 export const AppContext = ({ children }) => {
     const [store, setStore] = useState({
-        apiUrl: ' http://127.0.0.1:5000',
+        apiUrl: 'http://127.0.0.1:5000',
         access_token: null,
         user: null,
         JobCards: []
     });
-
-
     //estados que estoy usando temporalmente para testear los navbar al estar conectado o modo oscuro-franco
     const [logged, setLogged] = useState(false);
     const [darkMode, setDarkMode] = useState(false);
-
     const [actions] = useState({
         checkUser: async () => {
-            if (sessionStorage.getItem('access_token')) {
-                setStore((store) => ({ ...store, access_token: sessionStorage.getItem('access_token') }))
-                setStore((store) => ({ ...store, user: JSON.parse(sessionStorage.getItem('user')) }))
+            if (sessionStorage.getItem('access_token') == !undefined) {
+                console.log(sessionStorage.getItem('access_token'))
+                setStore((store) => ({
+                    ...store,
+                    access_token: sessionStorage.getItem('access_token'),
+                    user: JSON.parse(sessionStorage.getItem('user'))
+                }))
+                setLogged(true)
             }
         },
-
         register: async (credentials) => {
             try {
                 const { apiUrl } = store
@@ -37,53 +33,16 @@ export const AppContext = ({ children }) => {
                     }
                 })
                 const datos = await response.json()
-
                 console.log(datos)
-
-
             }
             catch (error) {
                 console.log(error.message)
             }
         },
-
         login: async (credentials) => {
             try {
-                const { apiURL } = store
-                const response = await fetch(`${apiURL}/api/login`, {
-                    method: 'POST',
-                    body: JSON.stringify(credentials),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                const data = await response.json()
-
-                if (response.ok) {
-                    sessionStorage.setItem('access_token', data.access_token);
-                    sessionStorage.setItem('user', JSON.stringify(data.user));
-
-                    setStore((store) => ({
-                        ...store, access_token: data.access_token, user: data.user
-                    }))
-
-                    setLogged(true);
-                    return true;
-                } else {
-                    console.error(data.message);
-                    return false
-                }
-
-            } catch (error) {
-                console.log(error.message)
-                return false
-            }
-        },
-
-        jobposting: async () => {
-            try {
-                const { apiURL } = store
-                const response = await fetch(`${apiURL}/api/login`, {
+                const { apiUrl } = store
+                const response = await fetch(`${apiUrl}/api/login`, {
                     method: 'POST',
                     body: JSON.stringify(credentials),
                     headers: {
@@ -91,27 +50,48 @@ export const AppContext = ({ children }) => {
                     }
                 })
                 const datos = await response.json()
-
+                console.log(datos.data)
+                console.log(response.ok)
+                setStore((store) => ({
+                    ...store, access_token: datos.data.access_token, user: datos.data.user
+                }))
+                sessionStorage.setItem('access_token', datos.data.access_token);
+                sessionStorage.setItem('user', JSON.stringify(datos.data.user));
+                setLogged(true);
+                return true;
+            } catch (error) {
+                console.log(error.message)
+                return false
+            }
+        },
+        jobposting: async (credentials) => {
+            try {
+                const { apiUrL } = store
+                const response = await fetch(`${apiUrL}/api/login`, {
+                    method: 'POST',
+                    body: JSON.stringify(credentials),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                const datos = await response.json()
                 console.log(datos)
-
             } catch (error) {
                 console.log(error.message)
             }
         },
-
         updateProfile: async (formData, access_token) => {
             try {
-                const { apiUrl } = store
+                const { apiUrL } = store
                 console.log(access_token)
-                const response = await fetch(`${apiUrl}/api/profile`, {
-                    method: 'PATCH',
+                const response = await fetch(`${apiUrL}/api/profile`, {
+                    method: 'PUT',
                     body: formData,
                     headers: {
                         'Authorization': `Bearer ${access_token}`
                     }
                 })
                 const datos = await response.json()
-
                 console.log(datos)
                 if (datos.status === 'success') {
                     setStore((store) => ({ ...store, user: datos.user }))
@@ -120,12 +100,10 @@ export const AppContext = ({ children }) => {
                 } else {
                     return false
                 }
-
             } catch (error) {
                 console.log(error.message)
             }
         },
-        
         logout: () => {
             console.log('log out')
             sessionStorage?.removeItem('access_token');
@@ -135,20 +113,12 @@ export const AppContext = ({ children }) => {
         }
     }
     )
-
-
-
     useEffect(() => {
         actions.checkUser()
-        if (sessionStorage.getItem('user')) {
-            setLogged(true)
-        }
     }, [])
-
     return (
         <Context.Provider value={{ store, logged, darkMode, actions }}>
             {children}
         </Context.Provider>
     );
-
 }
