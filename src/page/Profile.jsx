afd2cb8b22487f4d4e909cb85a474f1e74140b97
AnahiVera <vera.anahi.93@gmail.com>
2024-10-18 15:22:20 -0300

    profile appearance

--- src/page/Profile.jsx
+++ src/page/Profile.jsx
@@ -37,34 +37,34 @@ const Profile = () => {
     <div className="container mt-5 py-3">
       {!loaded ? (
         <div><h1>Loading...</h1></div>
-      ) : (<div className="row text-center">
+      ) : (<div className="row ">
         <div className="col-md-5">
-          <h2 className=" mt-5">{store?.user?.username}</h2>
+          <h2 className=" mt-5 ms-5">{store?.user?.username}</h2>
           <div className="mx-auto my-1 p-2">
             <img src={store?.user?.profile?.avatar || imgSrc} alt="Profile Avatar" className='img-fluid w-50 my-3 profile-avatar rounded-circle' />
           </div>
-          <div className="d-flex justify-content-center align-itmes-center mb-3">
-            <FaEnvelope className='fs-4 me-1' />
+          <div className="d-flex justify-content-start mb-3">
+            <FaEnvelope className='fs-4 mx-4' />
             <h5>{store?.user?.email}</h5>
           </div>
-          <div className="d-flex justify-content-center align-itmes-center mb-3">
-            <FaBookOpen className='fs-4 me-1' />
+          <div className="d-flex justify-content-start  mb-3">
+            <FaBookOpen className='fs-4 mx-4' />
             <h5>{store?.user?.profile?.biography || "No biography"} </h5>
           </div>
-          <div className="d-flex justify-content-center align-itmes-center mb-3">
-            <FaPhone className='fs-4 me-1' />
+          <div className="d-flex justify-content-start  mb-3">
+            <FaPhone className='fs-4 mx-4' />
             <h5>{store?.user?.profile?.phone || "No phone number"} </h5>
           </div>
-          <div className="d-flex justify-content-center align-itmes-center mb-3">
-            <FaLocationDot className='fs-4 me-1' />
+          <div className="d-flex justify-content-start  mb-3">
+            <FaLocationDot className='fs-4 mx-4' />
             <h5>{store?.user?.profile?.country || "No country added"} </h5>
           </div>
-          <div className="d-flex justify-content-center align-itmes-center mb-3">
-            <FaGithub className='fs-4 me-1' />
+          <div className="d-flex justify-content-start  mb-3">
+            <FaGithub className='fs-4 mx-4' />
             <h5 > {store?.user?.profile?.github || "No Github link"} </h5>
           </div>
-          <div className="d-flex justify-content-center align-itmes-center mb-3">
-            <FaLinkedin className='fs-4 me-1' />
+          <div className="d-flex justify-content-start  mb-3">
+            <FaLinkedin className='fs-4 mx-4' />
             <h5>{store?.user?.profile?.linkedin || "No LinkedIn link"}</h5>
           </div>
 
