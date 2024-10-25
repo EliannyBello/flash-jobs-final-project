26fcdba92e7105dba263439f1a86b19a2c2e3fb1
fgamester <falvarez.munoz@hotmail.es>
2024-10-25 12:10:30 -0300

    profile postcards simplifyed

--- src/components/jobCards.jsx
+++ src/components/jobCards.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/JobCard.css'
 import { } from "react-icons/fa";
 import PostCard from './PostCard';
+import ProfilePostCard from './ProfilePostCard';
 import { useLocation } from 'react-router-dom';
 
 const JobCards = ({ data }) => {
@@ -10,9 +11,15 @@ const JobCards = ({ data }) => {
 
 	return (
 		<div className={"jobCard " + ((location.pathname == '/') && 'd-flex row row-cols-md-2 row-cols-xxl-3 g-2 justify-content-start')}>
-			{data?.map((datos, index) => (
-				<PostCard datos={datos} data={data} key={index} />
-			))}
+			{(location.pathname == '/') ? (
+				data?.map((datos, index) => (
+					<PostCard datos={datos} data={data} key={index} />
+				))
+			) : (
+				data?.map((datos, index) => (
+					<ProfilePostCard datos={datos} data={data} key={index} />
+				))
+			)}
 		</div>
 	)
 }
